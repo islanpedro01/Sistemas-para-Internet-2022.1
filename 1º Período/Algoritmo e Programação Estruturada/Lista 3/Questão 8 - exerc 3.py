3417f1349027eb7f079da58dc9778093854f2bdf
Islan Pedro <113214808+islanpedro01@users.noreply.github.com>
2022-09-20 14:41:49 -0300

    Update Questão 8 - exerc 3.py

--- 1º Período/Algoritmo e Programação Estruturada/Lista 3/Questão 8 - exerc 3.py
+++ 1º Período/Algoritmo e Programação Estruturada/Lista 3/Questão 8 - exerc 3.py
@@ -1,9 +1,9 @@
 N = int (input("Digite um número inteiro: "))
 
-i = 0
+i = 1
 
-for i in range (1, N, 1):
+for i in range (1, N+1, 1):
     qper = i*i
     if qper <= N:
         maior = qper
-print (f'O maior quadrado perfeito menor que {N} é {maior}')
+print (f'O maior quadrado perfeito menor ou igual a {N} é {maior}')
