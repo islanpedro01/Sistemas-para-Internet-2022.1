cbadb8efaa0eb277955c6ee209a029bf4b4eeb26
Islan Pedro <113214808+islanpedro01@users.noreply.github.com>
2022-09-22 17:46:26 -0300

    Update Questão 6 - exerc 4.py

--- 1º Período/Algoritmo e Programação Estruturada/Lista 4/Questão 6 - exerc 4.py
+++ 1º Período/Algoritmo e Programação Estruturada/Lista 4/Questão 6 - exerc 4.py
@@ -3,8 +3,8 @@ zé = 1.10
 ano = 0
 
 while zé <= chico:
-    zé += 0.02
-    chico += 0.03
+    zé += 0.03
+    chico += 0.02
     ano += 1
     
 print (f'Zé levará {ano} anos para ficar maior que Chico!')
