79e366730e3c8c7a459cc53adad90f9da372a61a
islanpedro01 <113214808+islanpedro01@users.noreply.github.com>
2023-04-10 22:05:53 -0300

    Exercício adicionado
    Gerador de lista utilizando javascript

---
+++ 2º Período/Linguagem de Script/Exerc - List-generator/js/main.js
@@ -0,0 +1,16 @@
+// TODO
+const form = document.querySelector('form')
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const num = Number(document.querySelector('.quantity').value)
+    const numplace = document.querySelector('.list');
+    let result = '';
+
+   for (let i = 0; i < num; i++) {
+        result += `<li>Item ${i+1}</li>`
+        console.log(result)
+    }
+    numplace.innerHTML = result;
+};
