022a7d50ef4b7bffb43a9f792e6b9b5308e391c2
islanpedro01 <113214808+islanpedro01@users.noreply.github.com>
2023-04-19 02:30:29 -0300

    Exercício adicionado

--- 2º Período/Linguagem de Script/Exerc - Calc-IMC/js/main.js
+++ 2º Período/Linguagem de Script/Exerc - Calc-IMC/js/main.js
@@ -1,35 +1,44 @@
 function calc(alt,peso,sexo){
 
     let IMC = peso/(alt*alt)
-    let result;
+    let result; 
     const IMCresult = document.querySelector('#IMCresult')
+    IMCresult.classList.add("border","text-white")    
     
+
     if ((!sexo &&  IMC < 19.1) || (sexo && IMC < 20.7)){
         result = "Abaixo do Peso"
-        IMCresult.classList.add("border","border-warning","bg-warning","text-white")
+        IMCresult.classList.remove("border-sucess","bg-sucess","border-danger","bg-danger")
+        IMCresult.classList.add("border-warning","bg-warning")
         return result;
 
     }
     else if ((!sexo && (IMC >= 19.1 && IMC < 25.8)) || (sexo && (IMC >= 20.7 && IMC < 26.4))){
         result = "Peso Normal"
-        IMCresult.classList.add("border", "border-sucess", "bg-success", "text-white")
+        IMCresult.classList.remove("border-warning","bg-warning","border-danger","bg-danger")
+        IMCresult.classList.add("border-sucess", "bg-success")
         return result;
     }
     else if ((!sexo && (IMC >= 25.8 && IMC < 27.3)) || (sexo && (IMC >= 26.4 && IMC < 27.8))){
         result = "Marginalmente Acima do Peso"
-        IMCresult.classList.add("border", "border-sucess", "bg-success", "text-white")
+        IMCresult.classList.remove("border-warning","bg-warning","border-danger","bg-danger") 
+        IMCresult.classList.add("border-sucess", "bg-success")     
         return result;
     }
     else if ((!sexo && (IMC >= 27.3 && IMC < 32.3)) || (sexo && (IMC >= 27.8 && IMC < 31.1))){
         result = "Acima do Peso Ideal"
-        IMCresult.classList.add("border","border-warning","bg-warning","text-white")
-
+        IMCresult.classList.remove("border-sucess","bg-sucess","border-danger","bg-danger")
+        IMCresult.classList.add("border-warning","bg-warning")
         return result;
     }
     else if ((!sexo && IMC >= 32.3) || (sexo && IMC >= 31.1)){
         result = "Obeso"
-        IMCresult.classList.add("border", "border-danger", "bg-danger", "text-white")
+        IMCresult.classList.remove("border-sucess","bg-sucess","border-warning","bg-warning")
+        IMCresult.classList.add("border-danger", "bg-danger")
         return result;
+    }
+    else{
+        return false;
     }
     
 }
@@ -42,24 +51,28 @@ button.onclick = () =>{
     const peso = Number(document.querySelector('#peso').value)
     const altura = Number(document.querySelector('#altura').value)
     const IMCresult = document.querySelector('#IMCresult')
-    if (peso == 0 || altura == 0){
-      IMCresult.value = 'Preencha os campos de Altura e Peso!'
-    }
-    else {
-        IMCresult.value = calc(altura, peso, sexo)
-    }
+    let error = document.querySelector('#warning')
 
+    if ((!calc(altura, peso, sexo))||(peso*altura == 0)){
+       error.classList.remove("d-none")
+       IMCresult.classList.remove("border","border-warning","bg-warning","text-white","border-sucess","bg-success","border-danger","bg-danger")
+       IMCresult.value = ''
+      }
+    else{
+        IMCresult.value = calc(altura, peso, sexo)
+        error.classList.add("d-none")
+    }
 }
 
 document.addEventListener('keyup', function(e){
 
+let entradas = document.querySelectorAll('input[type=text]')
 
     if (e.code === 'Enter'){
         button.click()
     }
     else if (e.code === 'Escape'){
-        peso.value = ''
-        altura.value = ''
-        IMCresult.value = ''
+        entradas.forEach(entry => entry.value = '') 
+        IMCresult.classList.remove("border","border-warning","bg-warning","text-white","border-sucess","bg-success","border-danger","bg-danger")
     }
 })
