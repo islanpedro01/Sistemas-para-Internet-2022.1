7f5dfd112c66be96b22fa488b8c4893c941e0179
islanpedro01 <113214808+islanpedro01@users.noreply.github.com>
2023-05-20 16:33:02 -0300

    CEP with Fetch exercise (using temp)

---
+++ 2º Período/Linguagem de Script/Exerc - CEP (fetch)/js/main.js
@@ -0,0 +1,53 @@
+const cepfield = document.querySelector('#cep')
+const rua = document.querySelector('#street')
+const bairro = document.querySelector('#neighborhood')
+const estado = document.querySelector('#state')
+const cidade = document.querySelector('#city')
+const erro = document.querySelector('#cepError')
+let temporizador
+
+async function getCEP(cep) {
+  if (cepfield.value!==''){
+    try {
+      const url = `https://viacep.com.br/ws/${cep}/json/`;
+      const response = await fetch(url);
+      const cepData = await response.json();
+      if (cepData.erro){
+        erro.classList.remove('hidden')
+        return;
+      }
+      rua.value = cepData.logradouro
+      bairro.value = cepData.bairro
+      estado.value = cepData.uf
+      cidade.value = cepData.localidade
+      erro.className ='hidden'    
+
+    } catch (error){
+      erro.classList.remove('hidden') 
+      clear();
+      
+    } 
+  } 
+  else{
+    erro.className ='hidden'
+    clear();
+  }
+}
+
+function clear(){
+  rua.value = ''
+  bairro.value = ''
+  estado.value = ''
+  cidade.value = '' 
+}
+
+
+
+
+  cepfield.addEventListener('input', function(){
+   clearTimeout(temporizador);
+   temporizador = setTimeout(()=>getCEP(cepfield.value),500);
+  //  Versão com onblur:
+  // cepfield.onblur = function(){getCEP(cepfield.value)}
+  })
+  
